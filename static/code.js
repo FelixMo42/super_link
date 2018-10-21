b4fce0fee2798d611796c1a9c91c6064b0ff4fcc
FelixMo42 <felix@tacocat.com>
2018-10-21 06:45:07 -0700

    fixed link mod bug and code miss save

--- static/code.js
+++ static/code.js
@@ -1,99 +1,285 @@
-<!doctype html>
-<html>
-	<head>
-		<title>super link</title>
+var Http = new XMLHttpRequest();
+const url = window.location.href
 
-		<script type="text/javascript" src="{{ url_for('static', filename='code.js') }}"></script>
-		<link rel= "stylesheet" type= "text/css" href= "{{ url_for('static', filename='style.css') }}">
+String.prototype.deentitize = function() {
+    var ret = this.replace(/&gt;/g, '>');
+    ret = ret.replace(/&lt;/g, '<');
+    ret = ret.replace(/&quot;/g, '"');
+    ret = ret.replace(/&apos;/g, "'");
+    ret = ret.replace(/&amp;/g, '&');
+	ret = ret.replace(/&#34;/g, '"');
+    return ret;
+};
 
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+//pop up funcs
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	</head>
-	<body>
-		<div class="header">
-			<a>load</a>
-			<a onclick="newVar()">new variable</a>
+function showPopup(popup) {
+	document.getElementById("shade").style.display = "block";
+	document.getElementById(popup).style.display = "block";
 
-			<a class="line"></a>
+	document.getElementById("shade").onclick = function() {
+		hidePopup(popup)
+	}
+}
 
-			<div class="dropdown">
-				<button class="dropbtn">basic math
-					<i class="fa fa-caret-down"></i>
-				</button>
-				<div class="dropdown-content">
-					<a onclick="newLink('add', '<input> + <input> = <input>')">add</a>
-					<a onclick="newLink('subtract', '<input> - <input> = <input>')">subtract</a>
-					<a onclick="newLink('multiply', '<input> * <input> = <input>')">multiply</a>
-					<a onclick="newLink('divide', '<input> / <input> = <input>')">divide</a>
-				</div>
-			</div>
+function hidePopup(popup) {
+	document.getElementById("shade").style.display = "none";
+	document.getElementById(popup).style.display = "none";
+}
 
-			<div class="dropdown">
-				<button class="dropbtn">units
-					<i class="fa fa-caret-down"></i>
-				</button>
-				<div class="dropdown-content">
-					<a onclick="newLink('temp_CeFa', '<input>°C = <input>°F')">celcius <> fahrenheit</a>
-					<a onclick="newLink('dist_MiKi', '<input>mi = <input>km')">miles <> kilometers</a>
-				</div>
-			</div>
+//calc funcs
 
-			<a class="line"></a>
+function update(name, value) {
+	data = {"name": name, "value": value}
 
-			<a href="/help">help</a>
-			<a href="/about">about</a>
-		</div>
+	Http.open("SET_VAR", url);
+	Http.send( JSON.stringify(data) );
+	Http.onreadystatechange = (e) => {
+		console.log(Http.responseText)
+		console.log(Http.readyState)
+		if (Http.responseText == "" || Http.readyState != 4) {
+			return
+		}
 
-		<div id="variables">
-			{% for name in variables %}
-				<span value="{{name}}" oncontextmenu="varMenu(this); return false;">
-					<span class="varName">{{name}}</span>:
-					<input
-						id="{{name}}"
-						value="{{value[name]}}"
-						placeholder="{{sets[name]}}"
-						onchange="update(this.id, this.value)"
-					>
-					<br>
-				</span>
-			{% endfor %}
-		</div>
+		try {
+			document.getElementById(name).classList.remove("wrong")
 
-		<hr>
+			data = JSON.parse(Http.responseText)
+		} catch(error) {
+			document.getElementById(name).classList.add("wrong")
 
-		<div id="links">
-			{% for name in links %}
-				<span class="link" id="{{name}}" type="{{links[name]['name']}}" oncontextmenu="linkMenu(this); return false;">
-					{{types[links[name]["name"]][-1] % tuple(list(links[name]["vars"]))}}
-					<br>
-				</span>
-			{% endfor %}
-		</div>
+			response = Http.responseText.split("\n")
+			alert(response[0])
+			data = JSON.parse(response[1])
+		}
 
-		<!-- POP UPS -->
+		console.log(data)
 
-		<div id="shade"></div>
-		<div class="popup" id="varNamer">
-			<label>Enter variable name:</label><br>
-			<input type="text" id="varNamer_name" placeholder="variable name">
-		</div>
+		for (var key in data) {
+			document.getElementById(key).placeholder = data[key]
+		}
+	}
+}
 
-		<div class="popup" id="varMenu">
-			<button id="varMenu_rename">rename</button><br>
-			<button id="varMenu_delete">delete</button>
-		</div>
+//var funcs
 
-		<div class="popup" id="linkEdit">
-			<span id="linkEdit_span"></span>
-			<br>
-			<button id="link_save" onclick="saveLink(this.parentNode)">save</button>
-		</div>
+function newVar(n = false) {
+	showPopup("varNamer")
+	document.getElementById("varNamer_name").select()
+	document.getElementById("varNamer_name").onchange = function() {
+		name = n || this.value
 
-		<div class="popup" id="linkMenu">
-			<button id="linkMenu_edit">edit</button><br>
-			<button id="linkMenu_delete">delete</button>
-		</div>
-	</body>
-</html>
+		if (name.replace(/\s+/g, '') == "") {
+			alert("You must enter a variable name!")
+			document.getElementById("varNamer_name").select()
+			return
+		}
+
+		if (document.getElementById(name)) {
+			alert("Name is allready taken!")
+			document.getElementById("varNamer_name").select()
+			return
+		}
+
+		varHTML  = "<span value='" + name + "' oncontextmenu='varMenu(this); return false;'>"
+		varHTML += " <span class='varName'>" + name + "</span>: "
+		varHTML += " <input id='" + name + "' value='' placeholder='' onchange='update(this.id, this.value)'>"
+		varHTML += " <br>"
+		varHTML += "</span>"
+
+		document.getElementById("variables").innerHTML += varHTML
+		hidePopup("varNamer")
+
+		Http.open("NEW_VAR", url);
+		Http.send( name );
+	}
+}
+
+function varMenu(el) {
+	showPopup("varMenu")
+	document.getElementById("varMenu_delete").onclick = function() {
+		el.parentNode.removeChild(el)
+		hidePopup("varMenu")
+
+		Http.open("DELETE_VAR", url);
+		Http.send( el.getAttribute("value") );
+	}
+	document.getElementById("varMenu_rename").onclick = function() {
+		hidePopup("varMenu")
+		showPopup("varNamer")
+		document.getElementById("varNamer_name").select()
+		document.getElementById("varNamer_name").onchange = function() {
+			if (this.value.replace(/\s+/g, '') == "") {
+				alert("You must enter a variable name!")
+				return
+			}
+
+			console.log(document.getElementById(this.value))
+			if (document.getElementById(this.value)) {
+				alert("Name is allready taken!")
+
+				document.getElementById("varNamer_name").select()
+				return
+			}
+
+			Http.open("RENAME_VAR", url);
+			Http.send(JSON.stringify({"old": el.getAttribute("value"), "new": this.value}));
+
+			hidePopup("varNamer")
+
+			for(var child = el.firstChild; child !== null; child = child.nextSibling) {
+				console.log(child)
+				if (child.nodeName == "SPAN") {
+					child.innerHTML = this.value
+				} else if (child.nodeName == "INPUT") {
+					child.id = this.value
+				}
+			}
+
+			el.setAttribute("value", this.value)
+		}
+	}
+}
+
+//link funcs
+
+function newLink(type, form) {
+	showPopup("linkEdit")
+	document.getElementById("linkEdit").setAttribute("type", type)
+	document.getElementById("linkEdit_span").innerHTML = form
+	document.getElementById("link_save").onclick = saveLink
+}
+
+function saveLink() {
+	el = this.parentNode
+
+	vars = {}
+	i = 0
+	ord = []
+	datas = []
+
+	for(var child = document.getElementById("linkEdit_span").firstChild; child !== null; child = child.nextSibling) {
+		if (child.nodeName == "INPUT") {
+			if (!document.getElementById(child.value)) {
+				datas.push(child.value)
+				ord.push(function() {
+					Http.open("NEW_VAR", url);
+					Http.send( datas[i] );
+					Http.onreadystatechange = (e) => {
+						if (Http.readyState == 4) {
+							console.log("yo", i)
+							i += 1
+							ord[i]()
+						}
+					}
+				})
+
+				varHTML  = "<span value='" + child.value + "' oncontextmenu='varMenu(this); return false;'>"
+				varHTML += " <span class='varName'>" + child.value + "</span>: "
+				varHTML += " <input id='" + child.value + "' value='' placeholder='' onchange='update(this.id, this.value)'>"
+				varHTML += " <br>"
+				varHTML += "</span>"
+
+				document.getElementById("variables").innerHTML += varHTML
+			}
+			vars[child.value] = ""
+		}
+	}
+
+	data = {"name": document.getElementById("linkEdit").getAttribute("type"), "vars": vars}
+	datas.push(data)
+	hidePopup("linkEdit")
+
+	ord.push(function() {
+		Http.open("NEW_LINK", url);
+		Http.send( JSON.stringify(datas[i]) );
+		Http.onreadystatechange = (e) => {
+			if (Http.responseText == "" || Http.readyState != 4) {
+				return
+			}
+
+			document.getElementById("links").innerHTML += Http.responseText.deentitize()
+		}
+	})
+
+	ord[i]()
+}
+
+function linkMenu(el) {
+	showPopup("linkMenu")
+
+	document.getElementById("linkMenu_edit").onclick = function() {
+		hidePopup("linkMenu")
+		showPopup("linkEdit")
+
+		html = ""
+
+		for(var child = el.firstChild; child !== null; child = child.nextSibling) {
+			if (child.nodeName == "SPAN") {
+				html += "<input value='" + child.innerHTML + "'></input>"
+			} else if (child.nodeName == "#text") {
+				console.log(child)
+				html += child.wholeText
+			}
+		}
+
+		document.getElementById("linkEdit_span").innerHTML = html
+		document.getElementById("link_save").onclick = function() {
+			el.parentNode.removeChild(el)
+			hidePopup("linkEdit")
+
+			Http.open("DELETE_LINK", url);
+			Http.send( el.id );
+			Http.onreadystatechange = (e) => {
+				console.log("YTGRRY", Http.readyState)
+
+				if (Http.readyState != 4) {
+					return
+				}
+
+				console.log("YTGRRY", "happen?")
+
+				vars = {}
+				name = el.getAttribute("type")
+
+				for(var child = document.getElementById("linkEdit_span").firstChild; child !== null; child = child.nextSibling) {
+					if (child.nodeName == "INPUT") {
+						vars[child.value] = ""
+					}
+				}
+
+				data = {"name": name, "vars": vars}
+
+				console.log(data)
+
+				Http.open("NEW_LINK", url);
+				Http.send( JSON.stringify(data) );
+				Http.onreadystatechange = (e) => {
+					if (Http.responseText == "" || Http.readyState != 4) {
+						return
+					}
+
+					console.log(Http.responseText)
+
+					document.getElementById("links").innerHTML += Http.responseText.deentitize()
+				}
+			}
+		}
+	}
+	document.getElementById("linkMenu_delete").onclick = function() {
+		el.parentNode.removeChild(el)
+		hidePopup("linkMenu")
+
+		for(var child = el.firstChild; child !== null; child = child.nextSibling) {
+			if (child.nodeName == "SPAN" && document.getElementById(child.innerHTML).value != "") {
+				value = document.getElementById(child.innerHTML).value
+				update(document.getElementById(child.innerHTML).id, "")
+				update(document.getElementById(child.innerHTML).id, value)
+				break
+			}
+		}
+
+		Http.open("DELETE_LINK", url);
+		Http.send( el.id );
+	}
+}
